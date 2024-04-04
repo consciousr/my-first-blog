d7705296ecee9ee76a7d56133266416845455691
consciousr <consciousreally@proton.me>
2024-04-04 03:11:59 +0300

    Added foler icons and  added bottons for manage posts.

--- gjsite/settings.py
+++ gjsite/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'gjsite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'consciouswork',
+        'USER': 'conscious',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
@@ -123,3 +127,6 @@ STATIC_ROOT = BASE_DIR / 'static'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/'
+
